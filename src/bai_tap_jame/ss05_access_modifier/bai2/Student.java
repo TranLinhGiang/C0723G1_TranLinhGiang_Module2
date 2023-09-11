8545557cd666f3ecad413972992dae79aa89d9b7
Trần Linh Giang <gt07839@gmail.com>
2023-09-11 10:28:53 +0700

    Giang_nop_bai_tap_ss05

--- src/bai_tap_jame/ss05_access_modifier/bai2/Student.java
+++ src/bai_tap_jame/ss05_access_modifier/bai2/Student.java
@@ -1,2 +1,37 @@
-package bai_tap_jame.ss05_access_modifier.bai2;public class Student {
+package bai_tap_jame.ss05_access_modifier.bai2;
+
+public class Student {
+    private String name;
+    private String classes;
+
+    public Student() {
+    }
+
+    public Student(String name, String classes) {
+        this.name = name;
+        this.classes = classes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+
+    public static class TestStudent {
+        public static void main(String[] args) {
+            Student student= new Student("Giang","C07");
+            System.out.println(student);
+        }
+    }
 }

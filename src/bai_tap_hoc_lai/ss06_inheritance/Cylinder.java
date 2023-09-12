00468bb5aa7655132efb8a41415d1232e1068233
Trần Linh Giang <gt07839@gmail.com>
2023-09-12 11:11:36 +0700

    Giang_nop_bai_tap_ss06

--- src/bai_tap_hoc_lai/ss06_inheritance/Cylinder.java
+++ src/bai_tap_hoc_lai/ss06_inheritance/Cylinder.java
@@ -1,13 +1,13 @@
 package bai_tap_hoc_lai.ss06_inheritance;
 
-public class Cylinder extends Circle {   // sử dụng extends để lớp Cylinder kế thừa lại lớp cỉcle.
+public class Cylinder extends Circle {   // sử dụng extends để lớp Cylinder kế thừa lại lớp circle.
     private double height;
 
     public Cylinder() {
     }
 
 
-    public Cylinder(double radius, String color, double height) {
+    public Cylinder(double radius, String color, double height) {   // đây là overload
         super(radius, color);     // Thành phần radius, color được kế thừa lại của lớp Circle, và thành phần tính diện tích không được kế thừa.
         this.height = height;
     }
@@ -20,7 +20,7 @@ public class Cylinder extends Circle {   // sử dụng extends để lớp Cyli
         this.height = height;
     }
 
-    public double getVolume() {        // đây là overload để mở rộng thêm hành vi tính thể tích
+    public double getVolume() {
         return super.getArea() * this.height;
     }
 
